e1cad359f3c16d962a58768607b06bb164425304
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-02-08 11:02:54 +0800

    Use db autoconfig

--- app/TodoProject/settings.py
+++ app/TodoProject/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'TodoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tododb',
-        'USER':'bear',
-        'PASSWORD':'IIIdevops123!',
-        'HOST':'django-postgresql-todo-master-db-postgresql-db-svc'
+        'NAME': os.environ['db_name'],
+        'USER': os.environ['db_username'],
+        'PASSWORD': os.environ['db_password'],
+        'HOST': os.environ['db_host']
+        # 'HOST': 'django-postgresql-todo-master-db-postgresql-db-svc'
     }
 }
 
